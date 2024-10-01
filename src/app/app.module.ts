be7f5aae785d72f4937e3f4765344e4e7dcbf9ef
jackson R <jacksonjack333r@gmail.com>
2024-10-01 12:35:14 +0530

    fix the angular fire error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,36 +4,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
-
+import { AdminModule } from './admin/admin.module';
 import { UserModule } from './user/user.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { provideAuth, getAuth } from '@angular/fire/auth';
 import { firebaseConfig } from '../enviroment/enviroment'; // Ensure the correct path
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    
+  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
- 
+    AdminModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
     UserModule,
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
   ],
-  providers: [
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),  // Move this to providers
-    provideFirestore(() => getFirestore()),                   // Move this to providers
-    provideAuth(() => getAuth()),                             // Move this to providers
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
